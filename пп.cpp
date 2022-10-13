c9286c2c531abf444510da413500c21130f940a3
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-10-13 21:20:40 +0300

    2

---
+++ пп.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a ;
+	int b ;
+	int c ;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	
+	int d = b * b - 4 * a * c;
+
+
+	if (d == 0) { cout << 1; }
+	else
+	{
+		cout << 2;
+	}
+}
+
